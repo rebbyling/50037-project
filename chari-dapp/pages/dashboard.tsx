dd3a9a74420bda957ef3c28c428acd8e6b962824
rebbyling <rebecca.lingzw@gmail.com>
2022-04-07 23:06:23 +0800

    UI update

--- chari-dapp/pages/dashboard.tsx
+++ chari-dapp/pages/dashboard.tsx
@@ -328,7 +328,7 @@ export default function Home() {
            </div>
          
                
-
+{/* 
         {currentAccount ? (
           <div className="w-full max-w-xs sticky top-3 z-50 ">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -386,7 +386,7 @@ export default function Home() {
             Connect Your Wallet
           </button>
 
-)}
+)} */}
 
 {allCoffee.map((coffee, index) => {
   return (

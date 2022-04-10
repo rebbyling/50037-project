c934f50b4e4f55e6731da1d1136c77a386833e97
invertedplant <invertedplant@gmail.com>
2022-04-10 18:16:33 +0800

    Uploaded Charity and Onboarding .sol files
    All relevant events and functions *should* be included.

---
+++ contracts/CustomCharityTest.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+import "hardhat/console.sol"; // if we're still planning on using hardhat
+// see if you can deploy the contract with CustomOnboarding
+
+contract CustomCharityTest{
+    address admin;  // Admin for the smart contract, could be admin of charity
+    address payable public charityaddress; // Where the payments actually go to
+    string public uen; // Charity's UEN, e.g. 12345678A
+    string public charityname; // For display purposes
+    uint256 donationcount;
+    address[] donors; // List of all charity donors 
+
+    // Table of all owners
+    mapping(address => bool) owners;
+
+    // Maps each donor's address with the total donation counts 
+    mapping(address => uint256) donationCounts;
+
+    // More advanced donation storing method that we probably don't need
+    // mapping(bytes32 => mapping(uint256 => Donation)) donations;
+    
+    constructor(address _admin, address payable _charityaddress, string memory _uen, string memory _charityname) {
+        // Create an instance of this contract for one charity
+        admin = _admin;
+        charityaddress = _charityaddress;
+        uen = _uen;
+        charityname = _charityname;
+        owners[_admin] = true;
+        owners[_charityaddress] = true;
+        // TODO: Check if making the constructor payable has any effect on security.
+    }
+
+    // Section for handling on-chain payments
+
+    event NewOnchainPayment(
+        address indexed from, // keyword indexed lets you search by the parameter
+        string message,
+        uint256 timestamp//,
+        // string name // omitted for the time being, privacy
+    );
+
+    struct OnchainPayment{
+        address donor;
+        string message;
+        uint256 timestamp;
+        // string name; // omitted for the time being, privacy
+    }
+
+    OnchainPayment[] onchainPayment; // Stores all the donations in an unindexed list, just in-order
+
+    function MakeOnchainPayment (string memory _message) public payable {
+        uint256 cost = 0.001 ether; // minimum amount to make sure gas doesn't eat the whole thing
+        require(msg.value >= cost, "Value too low to send!");
+
+        console.log("Making onchain transfer...");
+    
+        require(msg.sender.balance >= msg.value, "You don't have enough ether for this!");
+        (bool success, ) = charityaddress.call{value: msg.value}("");
+        require(success, "Failed to send money");
+
+        // only update the list after the transfer is complete
+        donationcount += 1;
+        onchainPayment.push(OnchainPayment(msg.sender, _message, block.timestamp));
+        emit NewOnchainPayment(msg.sender, _message, block.timestamp);
+
+        // update other useful stuff
+        uint size = donationCounts[msg.sender];
+        if (size == 0){
+            donors.push(msg.sender);
+        }
+        donationCounts[msg.sender] = size + 1;
+    }
+
+    // Helper functions
+
+    function GetOnlinePayments() public view returns (OnchainPayment[] memory){
+        // Returns the whole list of transactions.
+        return onchainPayment;
+    }
+
+    function GetNumOnlinePayments() public view returns (uint256){
+        // This returns the number of payments received in total.
+        return donationcount;
+    }
+
+    function GetDonors() public view returns (address[] memory){
+        // This returns a list of all donors.
+        return donors;
+    }
+
+    function GetDonorDonations(address _address) public view returns (uint256){
+        // This returns the number of donations made by a single address. This does not account for the size of donations.
+        return donationCounts[_address];
+    }
+
+    // Admin functions
+    modifier isAdmin {
+      require(msg.sender == admin, "Only admin can access this function");
+      _;
+    }
+    
+    modifier isOwners {
+    require(owners[msg.sender], "Only owners can access this function");
+      _;
+    }
+
+    function addOwner(address owner) public isAdmin {
+      owners[owner] = true;
+    }
+
+    function checkOwner(address owner) public view returns (bool) {
+      return owners[owner];
+    } 
+
+    function removeOwner(address owner) public isAdmin {
+      owners[owner] = false;
+    }
+
+    function updateAddress(address payable _oldaddress, address payable _newaddress) public isAdmin {
+        require(charityaddress == _oldaddress, "Addresses do not match!");
+        owners[_oldaddress] = false;
+        owners[_newaddress] = true;
+        charityaddress = _newaddress;
+    }
+
+    function updateName(string memory _newname) public isOwners returns(string memory) {
+        charityname = _newname;
+        return charityname;
+    }        
+
+}
---
+++ contracts/CustomOnboarding.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+import "./CustomCharityTest.sol"; // please modify this if needed!
+
+contract CustomOnboarding{
+    address private admin;
+
+
+    /*
+        If you're wondering why this has to be done like this, it's because of how Solidity handles mappings.
+    */
+    struct Charity{
+        address cAddress;
+        bool exists;
+    }
+
+    mapping (string => Charity) private charities; // map UEN to charity address
+
+    constructor(address _admin) {
+        admin = _admin;
+    }
+  
+    modifier isAdmin {
+        require(msg.sender == admin, "Only admin can access this function");
+        _;
+    }
+
+    /* 
+        This allows a web3 hook to pick up on the address of the new contract created during onboarding.
+        The event will be emitted if contract creation is successful. Returning the new address as part
+        of the function does not work. It's an oddity of solidity.
+        See https://ethereum.stackexchange.com/questions/45502/get-address-of-new-contract-from-contract-factory
+    */
+    event ContractCreated (address NewContractAddress);
+
+    // This onboards the charity.
+    function OnboardCharity(address CharityAdmin, 
+    address payable CharityAddress, 
+    string memory UEN, 
+    string memory CharityName) public isAdmin {
+        address ContractAddress = address(new CustomCharityTest(CharityAdmin, CharityAddress, UEN, CharityName));
+        charities[UEN].cAddress = CharityAddress;
+        charities[UEN].exists = true;
+        emit ContractCreated(ContractAddress);
+    }    
+
+    function UpdateCharityAddress(string memory UEN, address newAddress) public isAdmin{
+        require(charities[UEN].exists == true, "This charity does not exist!");
+        charities[UEN].cAddress = newAddress;
+    }
+
+    function getCharityAddress(string memory UEN) public view returns (address){
+        return charities[UEN].cAddress;
+    }
+}
